1167880e3cfb223512e286b0b84b919c6bec3a30
Michael A. Damone <michael@thedamones.com>
2025-04-01 11:42:32 -0700

    Initial commit

---
+++ src/main/java/com/thedamones/fusionauth/jotd/JokeOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package com.thedamones.fusionauth.jotd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeOfTheDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thedamones/fusionauth/jotd/JokeOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedamones.fusionauth.jotd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
